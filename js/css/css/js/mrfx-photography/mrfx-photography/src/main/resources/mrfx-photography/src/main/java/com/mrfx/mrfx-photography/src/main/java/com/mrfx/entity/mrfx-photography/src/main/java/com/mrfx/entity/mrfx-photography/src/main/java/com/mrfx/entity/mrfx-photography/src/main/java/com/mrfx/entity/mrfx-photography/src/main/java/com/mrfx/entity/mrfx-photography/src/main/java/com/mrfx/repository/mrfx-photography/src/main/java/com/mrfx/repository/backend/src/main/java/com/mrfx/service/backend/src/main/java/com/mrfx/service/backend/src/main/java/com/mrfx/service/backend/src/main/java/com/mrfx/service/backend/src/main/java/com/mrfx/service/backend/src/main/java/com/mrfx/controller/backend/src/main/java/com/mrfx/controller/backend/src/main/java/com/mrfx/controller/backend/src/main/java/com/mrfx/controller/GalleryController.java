d457054b08712c24ca738b5c3da21fb023bcfb7f
himanshuprkwd <bittuofficial793@gmail.com>
2026-06-16 06:06:43 -0700

    Update GalleryController.java

--- js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/controller/backend/src/main/java/com/mrfx/controller/backend/src/main/java/com/mrfx/controller/backend/src/main/java/com/mrfx/controller/GalleryController.java
+++ js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/controller/backend/src/main/java/com/mrfx/controller/backend/src/main/java/com/mrfx/controller/backend/src/main/java/com/mrfx/controller/GalleryController.java
@@ -1,1 +1,32 @@
+package com.mrfx.controller;
 
+import com.mrfx.entity.GalleryCode;
+import com.mrfx.repository.GalleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/gallery")
+@CrossOrigin("*")
+public class GalleryController {
+
+    @Autowired
+    private GalleryRepository galleryRepository;
+
+    @PostMapping("/create")
+    public GalleryCode createGallery(@RequestBody GalleryCode gallery) {
+        return galleryRepository.save(gallery);
+    }
+
+    @GetMapping("/all")
+    public List<GalleryCode> getAll() {
+        return galleryRepository.findAll();
+    }
+
+    @GetMapping("/{code}")
+    public GalleryCode getByCode(@PathVariable String code) {
+        return galleryRepository.findByCode(code).orElse(null);
+    }
+}
