453dc7299223bdd03b2cd7913f3caeb390c00066
himanshuprkwd <bittuofficial793@gmail.com>
2026-06-16 05:50:00 -0700

    Update BookingRepository.java

--- js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/BookingRepository.java
+++ js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/BookingRepository.java
@@ -1,1 +1,12 @@
+package com.mrfx.repository;
 
+import com.mrfx.entity.Booking;
+import com.mrfx.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByCustomer(Customer customer);
+}
