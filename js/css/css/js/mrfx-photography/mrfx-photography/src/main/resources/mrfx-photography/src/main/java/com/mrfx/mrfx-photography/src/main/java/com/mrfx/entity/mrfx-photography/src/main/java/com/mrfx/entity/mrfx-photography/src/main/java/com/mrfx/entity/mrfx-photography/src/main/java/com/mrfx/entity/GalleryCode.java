5e4e619246dd13500cc49de63fd73bdc599cc87c
himanshuprkwd <bittuofficial793@gmail.com>
2026-06-16 05:42:18 -0700

    Update GalleryCode.java

--- js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/GalleryCode.java
+++ js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/GalleryCode.java
@@ -1,1 +1,25 @@
+package com.mrfx.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "gallery_codes")
+@Data
+public class GalleryCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiryDate;
+
+    @ManyToOne
+    private Customer customer;
+}
