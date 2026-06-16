7ac2264bc82477a00eedb01a82292398ab86c43a
himanshuprkwd <bittuofficial793@gmail.com>
2026-06-16 05:52:47 -0700

    Update CustomerRepository.java

--- js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/CustomerRepository.java
+++ js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/CustomerRepository.java
@@ -9,3 +9,15 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findByEmail(String email);
 } 
+package com.mrfx.repository;
+
+import com.mrfx.entity.Photo;
+import com.mrfx.entity.GalleryCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+    List<Photo> findByGalleryCode(GalleryCode galleryCode);
+}
