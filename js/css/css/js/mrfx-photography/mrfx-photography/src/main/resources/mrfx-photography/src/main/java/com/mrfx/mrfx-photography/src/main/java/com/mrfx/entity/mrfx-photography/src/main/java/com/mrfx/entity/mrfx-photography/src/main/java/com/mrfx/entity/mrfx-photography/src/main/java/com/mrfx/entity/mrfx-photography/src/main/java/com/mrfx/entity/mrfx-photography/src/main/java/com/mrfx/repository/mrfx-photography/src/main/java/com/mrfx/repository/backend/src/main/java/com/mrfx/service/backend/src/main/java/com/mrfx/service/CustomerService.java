da203604cd5ae85202cc3eb110d54d8f3a8738bc
himanshuprkwd <bittuofficial793@gmail.com>
2026-06-16 05:56:47 -0700

    Implement CustomerService for registration and login

--- js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/CustomerService.java
+++ js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/backend/src/main/java/com/mrfx/service/backend/src/main/java/com/mrfx/service/CustomerService.java
@@ -1,1 +1,36 @@
+package com.mrfx.service;
 
+import com.mrfx.entity.Customer;
+import com.mrfx.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public Customer register(Customer customer) {
+        customer.setPassword(encoder.encode(customer.getPassword()));
+        return customerRepository.save(customer);
+    }
+
+    public Optional<Customer> login(String email, String password) {
+        Optional<Customer> customerOpt = customerRepository.findByEmail(email);
+
+        if (customerOpt.isPresent()) {
+            Customer customer = customerOpt.get();
+
+            if (encoder.matches(password, customer.getPassword())) {
+                return Optional.of(customer);
+            }
+        }
+        return Optional.empty();
+    }
+}
