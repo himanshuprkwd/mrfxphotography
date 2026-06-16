5b872a5ccfaebcf250da8dca91a6b548cc7f8175
himanshuprkwd <bittuofficial793@gmail.com>
2026-06-16 05:55:43 -0700

    Update AdminService.java

--- js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/backend/src/main/java/com/mrfx/service/AdminService.java
+++ js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/entity/mrfx-photography/src/main/java/com/mrfx/repository/mrfx-photography/src/main/java/com/mrfx/repository/backend/src/main/java/com/mrfx/service/AdminService.java
@@ -1,1 +1,36 @@
+package com.mrfx.service;
 
+import com.mrfx.entity.Admin;
+import com.mrfx.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public Admin register(Admin admin) {
+        admin.setPassword(encoder.encode(admin.getPassword()));
+        return adminRepository.save(admin);
+    }
+
+    public Optional<Admin> login(String email, String password) {
+        Optional<Admin> adminOpt = adminRepository.findByEmail(email);
+
+        if (adminOpt.isPresent()) {
+            Admin admin = adminOpt.get();
+
+            if (encoder.matches(password, admin.getPassword())) {
+                return Optional.of(admin);
+            }
+        }
+        return Optional.empty();
+    }
+}
