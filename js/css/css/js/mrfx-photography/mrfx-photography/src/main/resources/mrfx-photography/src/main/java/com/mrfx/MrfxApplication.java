7adb6633e7a1beb7a4dff8192f5a5e9ed4c851ef
himanshuprkwd <bittuofficial793@gmail.com>
2026-06-16 05:30:40 -0700

    Update MrfxApplication.java

--- js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/MrfxApplication.java
+++ js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/MrfxApplication.java
@@ -1,1 +1,17 @@
+package com.mrfx;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrfxApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+            MrfxApplication.class,
+            args
+        );
+
+    }
+}
