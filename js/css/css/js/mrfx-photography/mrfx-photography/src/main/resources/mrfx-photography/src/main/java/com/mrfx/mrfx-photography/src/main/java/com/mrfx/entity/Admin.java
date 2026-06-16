4a3c099d7f0bb58ac35c271e6293532bd9e5eab2
himanshuprkwd <bittuofficial793@gmail.com>
2026-06-16 05:36:17 -0700

    Update Admin.java

--- js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/Admin.java
+++ js/css/css/js/mrfx-photography/mrfx-photography/src/main/resources/mrfx-photography/src/main/java/com/mrfx/mrfx-photography/src/main/java/com/mrfx/entity/Admin.java
@@ -1,1 +1,22 @@
+package com.mrfx.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "admins")
+@Data
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role = "ADMIN";
+}
